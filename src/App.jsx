457f029397f37d3fc33733d24c3a8e50b1c7e4b2
Renier Vargas <rvargas@fenacrep.org>
2023-06-29 18:06:50 -0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-indigo-300 text-2xl'>
+        Hello to this new coure of React!
+      </div>
+    </>
+  )
+}
+
+export default App
