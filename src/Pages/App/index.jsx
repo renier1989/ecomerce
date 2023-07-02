166cdfd50dbb63c44e20cf3309e380f019015b73
Renier Vargas <renier1989@gmail.com>
2023-07-02 10:45:25 -0500

    class 4 implementing react-route-dom

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, useRoutes } from 'react-router-dom';
 import './App.css'
 import {Home} from '../Home';
 import {MyAccount} from '../MyAccount';
@@ -6,17 +7,33 @@ import {MyOrders} from '../MyOrders';
 import {SignIn} from '../SignIn';
 import {NotFound} from '../NotFound';
 
+const AppRoutes = () => {
+  let routes = useRoutes([
+    {path: '/', element: <Home />},
+    {path: '/my-account', element: <MyAccount />},
+    {path: '/my-order', element: <MyOrder />},
+    {path: '/my-orders', element: <MyOrders />},
+    {path: '/sign-in', element: <SignIn />},
+    {path: '/*', element: <NotFound />},
+  ]);
+
+  return routes;
+}
+
 function App() {
 
   return (
-    <>
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <SignIn />
-      <NotFound />
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+    // <>
+    //   <Home />
+    //   <MyAccount />
+    //   <MyOrder />
+    //   <MyOrders />
+    //   <SignIn />
+    //   <NotFound />
+    // </>
   )
 }
 
