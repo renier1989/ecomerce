e5f832ec603e4b51360845094e3da0e29fc5273f
Renier Vargas <renier1989@gmail.com>
2023-07-02 12:42:45 -0500

    Class 8 , Consuming an API, to show products in the HomePage with the Card component

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,15 +1,15 @@
 
-function Card() {
+function Card(props) {
   return (
     <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
         <figure className="relative w-full h-4/5 mb-2">
-            <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg m-2 px-2">Electronics</span>
-            <img className="hover:transition hover:duration-500 hover:shadow-md hover:shadow-slate-400 rounded-lg w-full h-full object-cover" src="https://images.pexels.com/photos/14642112/pexels-photo-14642112.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg m-2 px-2">{props.data.category.name}</span>
+            <img className="hover:transition hover:duration-500 hover:shadow-md hover:shadow-slate-400 rounded-lg w-full h-full object-cover" src={props.data.images[0]} alt={props.data.title} />
             <div className="absolute top-0 right-0 flex justify-center items-center rounded-full w-6 h-6 m-2 p-1 bg-white/60">+</div>
         </figure>
         <p className="flex justify-between items-center px-1">
-            <span className="text-sm  font-light">Xbox Wireless controll</span>
-            <span className="text-lg font-medium">$400</span>
+            <span className="text-sm  font-light">{props.data.title}</span>
+            <span className="text-lg font-medium">${props.data.price} </span>
         </p>
     </div>
   )
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,18 +1,30 @@
 // import React from 'react'
 
-import Card from "../../Components/Card"
-import Layout from "../../Components/Layout"
+import { useState } from "react";
+import Card from "../../Components/Card";
+import Layout from "../../Components/Layout";
+import { useEffect } from "react";
 
 function Home() {
-  return (
+  const [items, setItems] = useState(null);
 
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
+
+  return (
     <Layout>
       home
-      <Card />
+      <div className="grid grid-cols-4 gap-5 w-full max-w-screen-lg">
+        {items?.map((item) => (
+          <Card data={item} key={item.id} />
+        ))}
+      </div>
     </Layout>
     // <div className='bg-green-100'>Home Page</div>
-
-  )
+  );
 }
 
-export {Home}
+export { Home };
