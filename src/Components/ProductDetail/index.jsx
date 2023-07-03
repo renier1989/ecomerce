ddb7e52a996b7ac3994aced2e1a92f59893121dd
Renier Vargas <renier1989@gmail.com>
2023-07-03 11:18:18 -0500

    Class 13, open and close the pruduct detail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,12 +1,14 @@
 import { XMarkIcon } from '@heroicons/react/24/solid'
+import { useEcom } from '../../Context';
 
 function ProductDetail() {
+  const ecom = useEcom();
   return (
-    <aside className="flex flex-col fixed right-0 border border-black rounded-lg w-[360px]    h-[calc(98vh-68px)] bg-white">
+    <aside className={`${ecom.openDetail ? 'flex' : 'hidden'} flex-col fixed right-0 border border-black rounded-lg w-[360px]    h-[calc(98vh-68px)] bg-white`}>
         <div className="flex justify-between items-center p-5">
             <h2 className="font-medium text-xl">Detail</h2>
             <div>
-                <XMarkIcon className="w-6 h-6 text-blue-800" ></XMarkIcon>
+                <XMarkIcon className="w-6 h-6 text-blue-800 cursor-pointer" onClick={()=>ecom.onCloseDetail()} ></XMarkIcon>
             </div>
             
         </div>
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,12 +4,19 @@ const EcomContext = createContext();
 
 function EcomProvider({children}) {
 
-    const [count, setCount] = useState(0);  
+    const [count, setCount] = useState(0);
+    const [openDetail, setOpenDetail]  = useState(false);  // State to handle open or close the detail
+
+    const onOpenDetail = () => setOpenDetail(true);
+    const onCloseDetail = () => setOpenDetail(false);
 
   return (
         <EcomContext.Provider value={{
             count, 
+            openDetail,
             setCount,
+            onOpenDetail,
+            onCloseDetail,
         }}>
             {children}
         </EcomContext.Provider>
